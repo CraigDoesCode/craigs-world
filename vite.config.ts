a30a12aa92d611d64e75c8fad9a038dae75d5824
Craig Norford <craig.a.norford@gmail.com>
2025-02-21 00:37:05 +0000

    ci: fix base is config

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "craigs-world",
+  base: "/craigs-world/",
   server: {
     open: true,
     port: 3000
