e328b12b0c4fe71865945ab11d5517e3f036c44c
Craig Norford <craig.a.norford@gmail.com>
2025-06-17 23:47:05 +0100

    feat: added place holder content

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,28 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [insult, setInsult] = useState("")
+
+  const generateInsult = async () => {
+    const newInsult = "Don't be silly you're great! no instults here, check back in a couple of weeks to see the progress \n silly goose 🪿"
+
+    setInsult(newInsult)
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>Craig's World</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+        <button onClick={() => generateInsult()}>
+          Insult Yourself
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          My site's (soon to be spotify project) is not ready yet, so in the mean time you can click this button to generate an insult.
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        {insult}
       </p>
     </>
   )
