977231b8091e05ee916430183d0a6588d850fdb2
Craig Norford <craig.a.norford@gmail.com>
2025-06-22 23:42:53 +0100

    fix: responsive nav bar and sections

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
 import Header from './components/header'
+import Playlist from './components/playlist'
+import Search from './components/search'
 function App() {
   const [insult, setInsult] = useState("")
+  const [isPreview, setIsPreview] = useState(false)
 
   const generateInsult = async () => {
     const newInsult = "Don't be silly you're great! no instults here, check back in a couple of weeks to see the progress \n silly goose 🪿"
@@ -10,22 +13,32 @@ function App() {
     setInsult(newInsult)
   }
 
+   useEffect(() => {
+     const params = new URLSearchParams(window.location.search);
+     if (params.get("preview") === "true") {
+       setIsPreview(true);
+     }
+   }, []);
+
   return (
     <>
       <Header />
-      <div className="card">
-        <button onClick={() => generateInsult()}>
-          Insult Yourself
-        </button>
-        <p>
-          My site (soon to be spotify project) is not ready yet, so in the mean time you can click this button to generate an insult.
-        </p>
+      <div className="main">
+        {isPreview && <Search />}
+        {isPreview && <Playlist />}
       </div>
-      <p className="read-the-docs">
-        {insult}
-      </p>
+      {!isPreview && (
+        <div className="card">
+          <button onClick={() => generateInsult()}>Insult Yourself</button>
+          <p>
+            My site (soon to be spotify project) is not ready yet, so in the
+            mean time you can click this button to generate an insult.
+          </p>
+        </div>
+      )}
+      <p className="read-the-docs">{insult}</p>
     </>
-  )
+  );
 }
 
 export default App
