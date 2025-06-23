70cc8176c256ec54a50e7595b52da98dc9239f6b
CraigDoesCode <craig@passentry.com>
2025-06-23 19:02:32 +0100

    feat: added search for teack name and artist with dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -13,11 +13,22 @@ interface PlaylistItem {
   trackName: string;
   artistName: string;
 }
+interface SearchItem {
+  trackName: string;
+  artistName: string;
+}
+
+const results = [
+  {trackName: "Jackie Onasis", artistName: "Sammie Rae & Friends"},
+  {trackName: "Superstition", artistName: "Stevie Wonder"},
+  {trackName: "I'm Yours", artistName: "Jason Mraz"}
+]
 
 function App() {
   const [insult, setInsult] = useState("")
   const [isPreview, setIsPreview] = useState(false)
   const [playlist] = useState<PlaylistItem[]>(playlistItems)
+  const [searchResults, setSearchResults] = useState<SearchItem[]>([])
 
   const generateInsult = async () => {
     const newInsult = "Don't be silly you're great! no instults here, check back in a couple of weeks to see the progress \n silly goose 🪿"
@@ -36,7 +47,7 @@ function App() {
     <>
       <Header />
       <div className="main">
-        {isPreview && <Search />}
+        {isPreview && <Search searchResults={searchResults} setSearchResults={setSearchResults} />}
         {isPreview && <Playlist playlistItems={playlist} />}
       </div>
       {!isPreview && (
--- src/components/search.tsx
+++ src/components/search.tsx
@@ -12,13 +12,20 @@ const response = [
   {trackName: "Superstition", artistName: "Stevie Wonder"},
   {trackName: "I'm Yours", artistName: "Jason Mraz"}
 ]
-export default function Search() {
-  const [searchResults] = useState<SearchItem[]>(response);
+export default function Search({ searchResults, setSearchResults }: { searchResults: SearchItem[], setSearchResults: (results: SearchItem[]) => void  }) {
 
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const query = e.target.value;
+    const filteredResults = response.filter(result => 
+      result.trackName.toLowerCase().includes(query.toLowerCase()) ||
+      result.artistName.toLowerCase().includes(query.toLowerCase())
+    );
+    setSearchResults(filteredResults);
+  }
 
   return (
     <div id="search">
-      <input id="search-input" type="text" placeholder="Search" />
+      <input id="search-input" type="text" placeholder="Search" onChange={handleSearch} />
       <div id="search-results">
         {searchResults && searchResults.map((result) => (
           <SearchItem key={result.trackName} trackName={result.trackName} artistName={result.artistName} />
